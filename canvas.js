047fb047b1307cddcb3a3b8691caec1406637f70
brunommst <38664310+brunommst@users.noreply.github.com>
2018-07-13 00:37:06 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,36 +2,42 @@ var canvas;
 var mic;
 
 function windowResized() {
-  //console.log('resized');
-  resizeCanvas(windowWidth, windowHeight);
+    //console.log('resized');
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
- 
-    
+
+
     var element = document.getElementById('centerside');
     var positionInfo = element.getBoundingClientRect();
-    
-var height = positionInfo.height;
-var width = $("#centerside").width();
-var top = positionInfo.top;
-var left = positionInfo.left;
-var right = positionInfo.right;
-//    console.log(width);
-    
-  canvas = createCanvas(width, height);
-  canvas.parent("centerside");
-  canvas.position(left+10, top);
-  canvas.style('z-index', '-1');
-  canvas.style('width', '100%');
 
+    var height = positionInfo.height;
+    var width = $("#centerside").width();
+    var top = positionInfo.top;
+    var left = positionInfo.left;
+    var right = positionInfo.right;
+
+    canvas = createCanvas(width, height);
+    canvas.parent("centerside");
+
+    if (windowWidth > 600) {
+        canvas.position(left+30, top);
+    } else {
+        canvas.position(left, top);
+    }
+    canvas.style('z-index', '-1');
+    canvas.style('width', '100vw');
+
+    frameRate(60);
+}
+
+
+function draw() {
+    ellipse(mouseX, mouseY, pmouseX, pmouseY);
 }
 
 function mousePressed() {
-  clear();
+    $("canvas")[0].getContext('2d').clearRect(0, 0, width, height);
 }
 
-function draw() {
-  ellipse(mouseX, mouseY, pmouseX, pmouseY);
-
-}
